687ac7b8748794a5a0dd144ae6169082c0eda501
JaimeGuti <jaime.m.guti@gmail.com>
2018-11-19 18:13:33 +0100

    El cliente ya se ejecuta de la manera indicada

--- client.py
+++ client.py
@@ -5,12 +5,13 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
-
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+METODO = sys.argv[1].upper()
+RECEPTOR = sys.argv[2].split("@")[0]
+IP = sys.argv[2].split("@")[1].split(":")[0]
+SIPPORT = int(sys.argv[2].split(":")[-1])
 
 # Contenido que vamos a enviar
 LINE = '¡Hola mundo!'
@@ -18,13 +19,17 @@ LINE = '¡Hola mundo!'
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((IP, SIPPORT))
 
     print("Enviando: " + LINE)
+    a = bytes(METODO, 'utf-8') + bytes(RECEPTOR, 'utf-8') + b'\r\n\r\n'
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
 
     print('Recibido -- ', data.decode('utf-8'))
     print("Terminando socket...")
+
+    if METODO == "INVITE":
+        print("ok invite")
 
 print("Fin.")
