4ea4bb79d85f95df2f6567b1145c24e9635e7da5
JaimeGuti <jaime.m.guti@gmail.com>
2018-11-19 18:22:07 +0100

    El servidor ya se ejecuta de la manera correcta

--- server.py
+++ server.py
@@ -5,7 +5,11 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
+AUDIO = sys.argv[3]
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -26,6 +30,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    serv = socketserver.UDPServer(('', 5555), EchoHandler)
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Servidor apagado")
