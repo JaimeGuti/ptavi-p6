721ce135b0686eb764d429a97db7eb16df93a624
JaimeGuti <jaime.m.guti@gmail.com>
2018-11-19 18:24:14 +0100

    El servidor ya responde con un mensaje de error en el caso de la introducción incorrecta de parámetros

--- server.py
+++ server.py
@@ -7,9 +7,12 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 
-IP = sys.argv[1]
-PORT = int(sys.argv[2])
-AUDIO = sys.argv[3]
+try:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    AUDIO = sys.argv[3]
+except:
+    sys.exit("Usage: python3 server.py IP port audio_file")
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
